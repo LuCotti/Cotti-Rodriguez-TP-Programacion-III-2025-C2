6714ed944e51b6f07d2f96347a3aba56cf9e89ec
LuCotti <lucianoarielcotti@gmail.com>
2025-10-18 12:18:28 -0300

    Agregar elementos a las vistas y lógica de navegación

---
+++ front/controllers/carrito.js
@@ -0,0 +1,7 @@
+const sectionProductos = document.getElementById("productos");
+const btnFinalizarCompra = document.getElementById("finalizar-compra");
+
+btnFinalizarCompra.onclick = () => {
+  console.log("Click!");
+  window.location.replace("../views/ticket.html");
+}
---
+++ front/controllers/ticket.js
@@ -0,0 +1,5 @@
+const btnSalir = document.getElementById("salir");
+
+btnSalir.onclick = () => {
+  window.location.replace("../views/bienvenida.html");
+}
