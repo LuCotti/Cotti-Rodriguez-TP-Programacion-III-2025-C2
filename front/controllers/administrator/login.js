910c1fc59fc2736beb90b90c232964019044380a
Gonza PC - Temperley <gonz.r@hotmail.com>
2025-10-20 18:41:13 -0300

    se capturan datos del login de administrador. Primer commit

---
+++ front/controllers/administrator/login.js
@@ -0,0 +1,10 @@
+const btnIngresar = document.getElementById("ingresar");
+
+btnIngresar.addEventListener("click", () => {
+    
+    const mail = document.getElementById("inputMail").value;
+    const pass = document.getElementById("inputClave").value;
+    
+    
+});
+
