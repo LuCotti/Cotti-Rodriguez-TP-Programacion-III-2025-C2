6991aae65ea971391e212b61817160bd2f2f9abc
LuCotti <lucianoarielcotti@gmail.com>
2025-11-01 20:21:54 -0300

    Crear archivo para funciones y variables, y agregar funcionalidad a la vista del carrito.

--- front/controllers/client/carrito.js
+++ front/controllers/client/carrito.js
@@ -1,8 +1,37 @@
+import { mostrarGuardados } from './funciones-variables.js';
 const btnTema = document.getElementById("btn-tema");
-const sectionProductos = document.getElementById("section-productos");
 const btnFinalizarCompra = document.getElementById("btn-finalizar-compra");
 
 btnFinalizarCompra.onclick = () => {
-  console.log("Click!");
   window.location.replace("./ticket.html");
 }
+
+mostrarGuardados();
+
+// function mostrarGuardados() {
+//   eliminarElementos(sectionProductos);
+//   const productos = traerGuardados();
+//   if (productos.length === 0) {
+//     sectionProductos.innerText = "No hay productos en el carrito";
+//   } else {
+//     sectionProductos.innerText = "";
+//     for (let p of productos) {
+//       const div = crearCard(p);
+//       sectionProductos.appendChild(div);
+
+//       if (estaGuardado(p.id)) {
+//         const btnQuitar = document.getElementById(`btn-quitar-${p.id}`);
+//         btnQuitar.addEventListener("click", () => {
+//           quitarProducto(p);
+//           window.location.reload();
+//         });
+//       } else {
+//         const btnAgregar = document.getElementById(`btn-agregar-${p.id}`);
+//         btnAgregar.addEventListener("click", () => {
+//           guardarProducto(p);
+//           window.location.reload();
+//         });
+//       }
+//     }
+//   }
+// }
