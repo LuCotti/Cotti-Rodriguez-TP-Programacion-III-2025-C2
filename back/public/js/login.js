e4b3629ba95f2d533226602b1b7619ac158f21f1
LuCotti <lucianoarielcotti@gmail.com>
2025-11-04 20:06:21 -0300

    Agregar botón de acceso rápido al login

--- back/public/js/login.js
+++ back/public/js/login.js
@@ -1,41 +1,46 @@
+const inputMail = document.getElementById("input-mail");
+const inputClave = document.getElementById("input-clave");
 const btnIngresar = document.getElementById('btn-ingresar');
+const btnAccesoRapido = document.getElementById("btn-acceso-rapido");
 
- btnIngresar.addEventListener("click", async() => {
-   const mail = document.getElementById("input-mail").value;
-   const pass = document.getElementById("input-clave").value;
-   const mensaje = document.getElementById("p-mensaje");
-   if (mail.length === 0 || pass.length === 0) {
-     mensaje.innerText = "Por favor, ingrese todos los datos...";
-   } else {
-     mensaje.innerText = "";
+btnAccesoRapido.addEventListener("click", () => {
+  inputMail.value = "gonza@admin.com";
+  inputClave.value = "Inicio1234.";
+});
 
-     try {
-       const response = await fetch('http://localhost:3000/administrator', {
-         method: 'POST',
-         headers: {
-           'Content-Type': 'application/json'
-         },
-         body: JSON.stringify({ mail, pass })
-       });
+btnIngresar.addEventListener("click", async() => {
+  const mail = document.getElementById("input-mail").value;
+  const pass = document.getElementById("input-clave").value;
+  const mensaje = document.getElementById("p-mensaje");
+  if (mail.length === 0 || pass.length === 0) {
+    mensaje.innerText = "Por favor, ingrese todos los datos...";
+  } else {
+    mensaje.innerText = "";
 
+    try {
+      const response = await fetch('http://localhost:3000/administrator', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ mail, pass })
+      });
 
-       if (response.ok) {
-         // Éxito (status 200)
-         const data = await response.json();
+      if (response.ok) {
+        // Éxito (status 200)
+        const data = await response.json();
 
+        window.location.href = './dashboard.ejs'; // Redirige al panel de usuario
+      } else {
+        // Error (Ej: status 401 o 400)
+        const errorData = await response.json();
+        mensaje.innerText = errorData.error || 'Email o contraseña incorrectos';
+      }
 
-         window.location.href = './dashboard.ejs'; // Redirige al panel de usuario
-       } else {
-         // Error (Ej: status 401 o 400)
-         const errorData = await response.json();
-         mensaje.innerText = errorData.error || 'Email o contraseña incorrectos';
-       }
-
-     } catch (error) {
-       // Error de red o algo falló en la conexión
-       console.error('Error de red:', error);
-       mensaje.innerText = 'No se pudo conectar al servidor. Inténtalo de nuevo.';
-     }
-   }
-
-   });
+    } catch (error) {
+      // Error de red o algo falló en la conexión
+      console.error('Error de red:', error);
+      mensaje.innerText = 'No se pudo conectar al servidor. Inténtalo de nuevo.';
+    }
+  }
+});
