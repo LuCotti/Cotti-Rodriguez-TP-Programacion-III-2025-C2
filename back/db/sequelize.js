65e175875c30fb4dd69a97bc3e3b0beca15bd446
LuCotti <lucianoarielcotti@gmail.com>
2025-11-25 19:11:24 -0300

    Cambiar paquete a sqlite3 y conexión con la base de datos

--- back/db/sequelize.js
+++ back/db/sequelize.js
@@ -1,7 +1,11 @@
+const path = require('path');
 const { Sequelize } = require("sequelize");
 const stringDb = process.env.STRING_DB;
-const sequelize = new Sequelize(stringDb);
-
+// const sequelize = new Sequelize(stringDb);
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: path.join(__dirname, 'database.sqlite'),
+});
 sequelize.sync();
 
 module.exports = sequelize;
