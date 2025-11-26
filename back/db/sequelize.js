544be79fdd59e61ae93240e1a23aefab9a4fe79c
Gonza PC - Temperley <gonz.r@hotmail.com>
2025-11-26 17:28:46 -0300

    Se vuelve a cambiar a mysql2

--- back/db/sequelize.js
+++ back/db/sequelize.js
@@ -2,10 +2,7 @@ const path = require('path');
 const { Sequelize } = require("sequelize");
 const stringDb = process.env.STRING_DB;
 // const sequelize = new Sequelize(stringDb);
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, 'database.sqlite'),
-});
+const sequelize = new Sequelize(stringDb);
 sequelize.sync();
 
 module.exports = sequelize;
