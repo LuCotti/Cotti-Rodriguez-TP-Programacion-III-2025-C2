6d785c0731bb656bccbb889f5bfc24101e18a99c
LuCotti <lucianoarielcotti@gmail.com>
2025-12-24 20:12:37 -0300

    chau multer

--- back/routes/producto.routes.js
+++ back/routes/producto.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 const router = Router();
-import upload from '../middlewares/multer.js';
+// import upload from '../middlewares/multer.js';
 import {
   crear,
   irACrear,
@@ -15,7 +15,7 @@ import {
 import { validarDatos } from '../middlewares/producto.js';
 
 // Crear un producto
-router.post('/', upload.single('imagen'), validarDatos, crear);
+// router.post('/', upload.single('imagen'), validarDatos, crear);
 
 // Redireccionar
 router.get('/alta', irACrear);
@@ -27,7 +27,7 @@ router.get('/all', traerTodos);
 router.get('/', traerTodosConPaginacion);
 
 // Modificar un producto
-router.put('/:id', upload.single('imagen'), modificar);
+// router.put('/:id', upload.single('imagen'), modificar);
 
 // Dar de baja un producto
 router.put('/activo/:id', darDeBaja);
